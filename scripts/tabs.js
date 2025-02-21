dd0b5ef6b8a010b24629cc648f161e582e60eef1
Telos <temoffy@gmail.com>
2025-02-20 20:48:05 -0600

    Add project files.

---
+++ scripts/tabs.js
@@ -0,0 +1,52 @@
+//Timothy Hopping, feb 2025, CC-BY-4 license, LeTU web dev class
+//parallel project draft 1
+
+//handles the 2 tab layers, credit to https://www.w3schools.com/howto/howto_js_tabs.asp
+
+document.write("Hello World");
+
+function openCity(evt, cityName) {
+  // Declare all variables
+  var i, tabcontent, tablinks;
+  
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+} 
+
+function openTab(evt, tabsetName, tabName) {
+
+  // Declare all variables
+  var i, tabset, tabcontent, tablinks;
+  
+  //don't touch any other tab systems!
+  tabset = document.getElementById(tabsetName);
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = tabset.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = tabset.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
